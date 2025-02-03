f51a8d85dee44b04fad6d53400cab3d43ea69ecc
Masudur400 <md8004210@gmail.com>
2025-02-03 15:19:45 +0600

    get user by email

--- index.js
+++ index.js
@@ -50,6 +50,14 @@ async function run() {
       res.send(result)
     })
 
+    // user get by email 
+    app.get('/users/:email', async (req, res)=>{
+      const email = req.params.email
+      const query = {email : email}
+      const result = await usersCollection.findOne(query)
+      res.send(result)
+    })
+
 
 
     // Send a ping to confirm a successful connection
