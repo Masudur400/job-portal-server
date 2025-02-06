16dc5bd3bb99b32c0baab345b2c2a4a4be959648
Masudur400 <md8004210@gmail.com>
2025-02-06 18:14:04 +0600

    jobs get by id

--- index.js
+++ index.js
@@ -92,6 +92,14 @@ async function run() {
       res.send(result)
     })
 
+    // job get by id
+    app.get('/jobs/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.findOne(query)
+      res.send(result)
+    })
+
 
 
 
